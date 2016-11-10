a752b8ad12c4e27c0ea068a51f8536d9852b1d8c
Donny Cheng <donccl@gmail.com>
2016-11-09 20:32:55 -0500

    adding postcard list view

--- ElectronicPostcard/app/src/main/java/com/gsu/electronicpostcard/PostcardListActivity.java
+++ ElectronicPostcard/app/src/main/java/com/gsu/electronicpostcard/PostcardListActivity.java
@@ -1,13 +1,82 @@
 package com.gsu.electronicpostcard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PostcardListActivity extends AppCompatActivity {
 
+    // Adaptor
+    public class PostcardListViewAdaptor extends ArrayAdapter<String> {
+
+        Context mContext;
+        ArrayList<String> mDataSource;
+
+        public PostcardListViewAdaptor(Context context, ArrayList<String> objects) {
+            super(context, R.layout.postcard_listview_item, objects);
+
+            mContext = context;
+            mDataSource = objects;
+        }
+
+        public int getCount()
+        {
+            return mDataSource.size();
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+
+            if (convertView==null){
+                // create view
+                convertView = inflater.inflate(R.layout.postcard_listview_item, parent, false);
+            }
+
+            return convertView;
+        }
+    }
+
+    ArrayList<String> dataSource;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postcard_list);
+
+        dataSource = new ArrayList<String>();
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+        dataSource.add("hello");
+
+        PostcardListViewAdaptor adaptor = new PostcardListViewAdaptor(getApplicationContext(), dataSource);
+        listView = (ListView)findViewById(R.id.postcard_listview);
+        listView.setAdapter(adaptor);
+
     }
+
+    public void onAddNewPostcard(View view){
+
+    }
+
+
 }
