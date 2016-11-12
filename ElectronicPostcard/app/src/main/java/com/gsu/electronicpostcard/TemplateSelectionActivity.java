8374872e695fecec328621a0adb1b07dc1023473
Donny Cheng <donccl@gmail.com>
2016-11-11 21:49:42 -0500

    adding template selection view

--- ElectronicPostcard/app/src/main/java/com/gsu/electronicpostcard/TemplateSelectionActivity.java
+++ ElectronicPostcard/app/src/main/java/com/gsu/electronicpostcard/TemplateSelectionActivity.java
@@ -1,13 +1,56 @@
 package com.gsu.electronicpostcard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gsu.fragments.TemplateSelectionFragment;
 
 public class TemplateSelectionActivity extends AppCompatActivity {
+
+
+    public class TemplateSelectionViewPagerAdapter extends FragmentPagerAdapter {
+
+
+        public TemplateSelectionViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            return (Fragment)TemplateSelectionFragment.newInstance(position);
+        }
+
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+
+    }
+
+
+    public ViewPager selectTempalteViewpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_template_selection);
+
+        setTitle("Select a template");
+
+        selectTempalteViewpager = (ViewPager)findViewById(R.id.select_template_viewpager);
+        TemplateSelectionViewPagerAdapter tsAdapter = new TemplateSelectionViewPagerAdapter(getSupportFragmentManager());
+        selectTempalteViewpager.setAdapter(tsAdapter);
+
     }
+
+
+
+
 }
