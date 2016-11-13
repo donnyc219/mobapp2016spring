80638a20e0b5429c03eb1ec27aeadd2ba10ebd38
Jack <nanotek2711@yahoo.com>
2016-11-12 20:14:56 -0500

    Added object structure for postcards

--- ElectronicPostcard/app/src/main/java/com/gsu/electronicpostcard/PostCardElement.java
+++ ElectronicPostcard/app/src/main/java/com/gsu/electronicpostcard/PostCardElement.java
@@ -1,15 +1,40 @@
 package com.gsu.electronicpostcard;
 
 import android.graphics.Bitmap;
+import android.graphics.Point;
 
 /**
  * Created by Jack on 11/11/2016.
  */
 
 public abstract class PostCardElement {
-    int positionX = 0, positionY = 0;
-    double scaleX = 1, scaleY = 1;
+    int positionX = 0, positionY = 0; // Position x and y are the coordinate of the center of the element.
+    int width, height;
+    double scale = 1;
     double rotation;
 
     abstract public void render(Bitmap bitmap);
+
+    // Method to get the bounding box of the object.
+    // The method returns an array of 4 points representing the 4 corners of the elements
+    // adjusted for position, size, scale and rotation.
+    public Point[] getBoundingBox() {
+        Point[] result = new Point[4];
+        result[0] = new Point(-width / 2, -height / 2);
+        result[1] = new Point(width / 2, -height / 2);
+        result[2] = new Point(width / 2, height / 2);
+        result[3] = new Point(-width / 2, height / 2);
+
+        for (Point corner : result) {
+            corner.x *= scale;
+            corner.y *= scale;
+            int tempX = corner.x;
+            corner.x = (int) (corner.x * Math.cos(rotation) - corner.y * Math.sin(rotation));
+            corner.y = (int) (corner.x * Math.cos(rotation) + corner.y * Math.sin(rotation));
+            corner.x += positionX;
+            corner.y += positionY;
+        }
+
+        return result;
+    }
 }
--- ElectronicPostcard/app/src/main/java/com/gsu/electronicpostcard/PostCardPage.java
+++ ElectronicPostcard/app/src/main/java/com/gsu/electronicpostcard/PostCardPage.java
@@ -1,6 +1,7 @@
 package com.gsu.electronicpostcard;
 
 import android.graphics.Bitmap;
+import android.graphics.Point;
 
 import java.util.ArrayList;
 
@@ -15,5 +16,21 @@ public class PostCardPage {
         for (PostCardElement element : elementList) {
             element.render(canvas);
         }
+    }
+
+    public PostCardElement getSelectedElement(int x, int y) {
+        for (int i = elementList.size() - 1; i >= 0; i++) {
+            Point[] corner = elementList.get(i).getBoundingBox();
+            boolean match = true;
+            for (int j = 0; j < corner.length; i++) {
+                Point vector1 = new Point(corner[j].x - x, corner[j].y - y);
+                Point vector2 = new Point(corner[(j + 1) % corner.length].x - corner[j].x,
+                        corner[(j + 1) % corner.length].y - corner[j].y);
+                int crossProduct = vector1.x * vector2.y - vector1.y * vector2.x;
+                if (crossProduct >= 0) match = false; // Mouse is outside of the bounding box
+            }
+            if (match) return elementList.get(i);
+        }
+        return null;
     }
 }
